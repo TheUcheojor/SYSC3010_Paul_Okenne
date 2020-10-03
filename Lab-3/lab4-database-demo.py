cf40fd1467918b754662e125e76443d17b25150e
Paul Okenne <paul.okenne@cmail.carleton.ca>
2020-10-02 22:05:53 -0400

    Added 2 two complement features to lab3-demo deliverable
    1) display all the sensors in the kitchen
    2)display all the door sensors.

--- Lab-3/lab4-database-demo.py
+++ Lab-3/lab4-database-demo.py
@@ -3,11 +3,20 @@ import sqlite3
 
 #connect to database file
 dbconnect = sqlite3.connect("mydatabase.db")
-
 dbconnect.row_factory = sqlite3.Row
 
 #now we create a cursor to work with 
 cursor = dbconnect.cursor()
+
+
+def printResult(results, message):
+
+    print(message+':\n')
+    # print(results[0]['id'])
+    for result in results:
+        print( str(result['sensorID'])+'---'+result['type']+'---'+result['zone'] )
+    print('\n')
+
 
 #Create sensors datatable
 cursor.execute('create table if not exists sensors(sensorID integer, type TEXT, zone TEXT ) ')
@@ -16,8 +25,20 @@ cursor.execute('create table if not exists sensors(sensorID integer, type TEXT, 
 rows=[ [1,'door', 'kitchen'],[2,'temperature', 'kitchen'],[3,'door', 'garage'],[4,'door', 'garage'],[5,'temperature', 'garage'] ]
 
 for row in rows:
-    print('insert into sensors values('+str(row[0])+',"'+row[1]+'","'+row[2]+'")')
     cursor.execute('insert into sensors values('+str(row[0])+',"'+row[1]+'","'+row[2]+'")')
     dbconnect.commit()
+
+
+# Displaying all the sensors in the kitchen
+rows=cursor.execute('SELECT * FROM sensors WHERE zone="kitchen"').fetchall()
+printResult(rows,' Displaying all the sensors in the kitchen ')
+
+# Displaying all the door sensors 
+rows=cursor.execute('SELECT * FROM sensors WHERE type="door"').fetchall()
+printResult(rows,'Displaying all the door sensors ')
+
+
+
+
 
 dbconnect.close()
