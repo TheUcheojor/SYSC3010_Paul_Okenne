140da3a7096d87b8f863e55408a9dc6e3c4447c7
Paul Okenne <paul.okenne@cmail.carleton.ca>
2020-10-05 13:17:59 -0400

    Changed Project Group Format and Output Response

--- Lab-4/lab4.py
+++ Lab-4/lab4.py
@@ -4,7 +4,7 @@ import requests
 URL='https://api.thingspeak.com/update?api_key='
 KEY='54CDA6FJF4RCJ92C'
 EMAIL = '&field1=paulokenne@cmail.carleton.ca'
-PROJECT_GROUP='&field2=L2_M_8'
+PROJECT_GROUP='&field2=L2-M-8'
 MEMBER_ID='&field3=b'
 
 NEW_URL=URL+KEY+EMAIL+PROJECT_GROUP+MEMBER_ID
@@ -12,4 +12,4 @@ NEW_URL=URL+KEY+EMAIL+PROJECT_GROUP+MEMBER_ID
 # Update Channel 
 get_data=requests.post(NEW_URL)
 
-print("response: "+ str(get_data) )
+print("post response: "+ str(get_data) +"\n")
